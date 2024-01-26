7b59eacd7b52c5a1265d5611d2eb768ee37f62f1
Karnsinh Chikane <karnsinh96@gmail.com>
2024-01-27 00:32:05 +0530

    Model Training Done

---
+++ src/KidneyDiseaseClassifier/pipeline/stage_03_model_training.py
@@ -0,0 +1,17 @@
+from KidneyDiseaseClassifier.config.configuration import ConfigurationManager
+from KidneyDiseaseClassifier.components.model_trainer import Training
+from KidneyDiseaseClassifier import logger
+
+STAGE_NAME = "Model Training"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
