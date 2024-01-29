f8874d0f26f0e8688910ed9f66b558bef4f9950c
Karnsinh Chikane <karnsinh96@gmail.com>
2024-01-29 16:04:35 +0530

    dvc.yaml File adde

--- src/KidneyDiseaseClassifier/components/data_inigestion.py
+++ src/KidneyDiseaseClassifier/components/data_inigestion.py
@@ -6,6 +6,7 @@ from KidneyDiseaseClassifier import logger
 from KidneyDiseaseClassifier.utils.common import get_size
 
 from KidneyDiseaseClassifier.config.configuration import DataIngestionConfig
+from KidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
@@ -41,3 +42,15 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> Data Ingestion for DVC check Started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> Data Ingestion for DVC check completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
